940b53fabf232dcc3cb289e8e7defd3ab8ea9bc0
kokarolos <kokarolos@gmail.com>
2020-05-03 00:47:44 +0300

    UnitOfWork and GenericRepository Implemented

---
+++ Loop/Loop.Services/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Loop.Services.Repositories_interface
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetById(int? id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        void Insert(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity); 
+    }
+}
---
+++ Loop/Loop.Services/Repositories/PostRepository.cs
@@ -0,0 +1,20 @@
+﻿using Loop.Database;
+using Loop.Entities;
+using Loop.Services.Repositories_interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop.Services.Repositories
+{
+    public class PostRepository : GenericRepository<Post>, IPostRepository
+    {
+        public PostRepository(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
