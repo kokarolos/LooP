e6318bec6e000d80031c432d8bbd71ac5074b19f
kokarolos <kokarolos@gmail.com>
2020-05-05 13:22:10 +0300

    gitIgnore Final Update

---
+++ Loop.Entities/Concrete/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Loop.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
