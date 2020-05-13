2a93a8aaa3443d48b1d6b683bfbf54882181aabd
panosrizos <pnsrzs@gmail.com>
2020-05-13 14:15:05 +0300

    TagsSelectedList

--- Loop.Services/Interfaces/Repositories/IPostRepository.cs
+++ Loop.Services/Interfaces/Repositories/IPostRepository.cs
@@ -1,9 +1,13 @@
 ﻿using Loop.Entities;
+using System.Collections.Generic;
 
 namespace Loop.Services.Repositories_interface
 {
     public interface IPostRepository :IRepository<Post>
     {
+        void Insert(Post post, IEnumerable<int> SelectedTagsIds);
+        void Update(Post post, IEnumerable<int> SelectedTagsIds);
+
 
     }
 }
