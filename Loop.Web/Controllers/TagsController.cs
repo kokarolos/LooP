3323bbddaa50c1c56e351c3635b8732f06d23f17
ioannis-grek <ioannis_1975@hotmail.com>
2020-05-23 12:52:47 +0300

    Tags: added(controller, view, css-js files) updated(navbar, tags in database)

---
+++ Loop.Web/Controllers/TagsController.cs
@@ -0,0 +1,28 @@
+﻿using Loop.Database;
+using Loop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loop.Web.Controllers
+{
+    public class TagsController : Controller
+    {
+        private readonly UnitOfWork db = new UnitOfWork(new ApplicationDbContext());
+
+        // GET: Tags
+        public JsonResult GetTags()
+        {
+            var tags = from tag in db.Tags.GetAll() select new {tag.Title, tag.Description, tag.ImageUrl };
+            return Json(tags, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
