bbee70608fc4cdda6abbe6e25a996c934b09683a
kokarolos <kokarolos@gmail.com>
2020-05-24 17:08:26 +0300

    Jobs Implemented

---
+++ Loop.Web/Controllers/JobsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Loop.Web.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
