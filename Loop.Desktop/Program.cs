f6ec0922439f8ab96ff8205ef5a198c913157921
kokarolos <kokarolos@gmail.com>
2020-05-26 01:02:32 +0300

    Dashboard Implementation

--- Loop.Desktop/Program.cs
+++ Loop.Desktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Loop.Database;
+using Loop.Entities;
 using Loop.Entities.Concrete;
 using Loop.Entities.Intermediate;
 using Loop.Services;
@@ -6,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Loop.Desktop
 {
@@ -64,8 +67,8 @@ namespace Loop.Desktop
             //}
 
 
-           //int rows = 0;
-           UnitOfWork db = new UnitOfWork(new ApplicationDbContext());
+            //int rows = 0;
+            UnitOfWork db = new UnitOfWork(new ApplicationDbContext());
             //
             //foreach (var user in db.Users.GetAll().ToList())
             //{
@@ -84,43 +87,98 @@ namespace Loop.Desktop
             //}
             //Console.WriteLine(rows);
 
-            var order = db.Orders.GetAll().ToList();
-            var op = db.OrderProducts.GetAll().ToList();
-            var op1 = new OrderProduct()
-            {
-                Price = 12,
-                Product = db.Products.GetById(1),
-                ProductId = 1,
-                Quantity = 5
-            };
-            var op2 = new OrderProduct()
-            {
-                Price = 125,
-                Product = db.Products.GetById(2),
-                ProductId = 2,
-                Quantity = 3
-            };
-            var op3 = new OrderProduct()
-            {
-                Price = 1,
-                Product = db.Products.GetById(3),
-                ProductId = 3,
-                Quantity = 1
-            };
-            var op4 = new List<OrderProduct>();
-            op4.Add(op1);
-            op4.Add(op2);
-            op4.Add(op3);
+            //var order = db.Orders.GetAll().ToList();
+            //var op = db.OrderProducts.GetAll().ToList();
+            //var op1 = new OrderProduct()
+            //{
+            //    Price = 12,
+            //    Product = db.Products.GetById(1),
+            //    ProductId = 1,
+            //    Quantity = 5
+            //};
+            //var op2 = new OrderProduct()
+            //{
+            //    Price = 125,
+            //    Product = db.Products.GetById(2),
+            //    ProductId = 2,
+            //    Quantity = 3
+            //};
+            //var op3 = new OrderProduct()
+            //{
+            //    Price = 1,
+            //    Product = db.Products.GetById(3),
+            //    ProductId = 3,
+            //    Quantity = 1
+            //};
+            //var op4 = new List<OrderProduct>();
+            //op4.Add(op1);
+            //op4.Add(op2);
+            //op4.Add(op3);
+            //
+            //foreach (var item in op4)
+            //{
+            //    Console.WriteLine(item.Product.Description);
+            //    db.OrderProducts.Insert(item);
+            //}
+            //var o = new Order() { OrderProducts = op4.ToList() ,OrderDate = DateTime.Now};
+            //db.Orders.Insert(o);
+            //db.Save();
 
-            foreach (var item in op4)
+
+
+            //
+            //var userId = "c925cc25-6b45-424b-832b-14b31fe9bdfb";
+            //var posts = db.Posts.GetAll().Where(post => post.ApplicationUserId == userId)
+            //                             .GroupBy(x => x.PostDate.Month)
+            //                             .Select(y => new
+            //                             {
+            //                                 key = y.Key,
+            //                                 count = db.Posts.GetAll()
+            //                                                 .Where(post => post.ApplicationUserId == userId && post.PostDate.Month == y.Key ).ToList()
+            //                             });
+            //
+            //
+
+
+            //var userId = "9fb01166-9c35-4be6-8fcd-7634e27e7932";
+            //var posts = db.Posts.GetAll()
+            //                 .ToList()
+            //                 .Where(post => post.ApplicationUserId == userId)
+            //                 .GroupBy(x => x.PostDate.Month)
+            //                 .Select(y => new
+            //                 {
+            //                     value = y.Key.ToString(),
+            //                     data = db.Posts.GetAll()
+            //                                     .ToList()
+            //                                     .Where(post => post.ApplicationUserId == userId && post.PostDate.Month == y.Key).ToList().Count.ToString()
+            //                 });
+            //
+            //foreach (var item in posts)
+            //{
+            //    Console.WriteLine($"{item.value} , {item.data}");
+            //}
+
+
+
+            //Get Replies Count from Posts of an user  and Group them By Tags Img.
+
+            var userId = "8089c1bf-0f5d-4175-850c-19b0a6eb3bf3";
+            var user = db.Users.GetUserById(userId);
+            var tagsPercentage = db.Posts.GetAll()
+                                          .ToList() //Posts that user replied
+                                          .Where(reply => reply.ApplicationUser == user)
+                                          .GroupBy(post => post)
+                                          .Select(x => new
+                                          {
+                                              tagTitle = x.Key.Tags.FirstOrDefault().Title,
+                                              repliesCount = x.Key.Replies.Where(g => g.ApplicationUser == user).Count()
+
+                                          });
+
+            foreach (var item in tagsPercentage)
             {
-                Console.WriteLine(item.Product.Description);
-                db.OrderProducts.Insert(item);
+                Console.WriteLine($"{item.tagTitle} : {item.repliesCount}");
             }
-            var o = new Order() { OrderProducts = op4.ToList() ,OrderDate = DateTime.Now};
-            db.Orders.Insert(o);
-            db.Save();
-
 
         }
     }
---
+++ Loop.Web/Controllers/DashboardController.cs
@@ -0,0 +1,83 @@
+﻿using Loop.Database;
+using Loop.Services;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Loop.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        private readonly UnitOfWork db = new UnitOfWork(new ApplicationDbContext());
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //Get Post Per User By Month
+        public JsonResult GetPosts()
+        {
+            var userId =  User.Identity.GetUserId();
+            var posts = db.Posts.GetAll()
+                                .ToList()
+                                .Where(post => post.ApplicationUserId == userId)
+                                .GroupBy(x => x.PostDate.Month)
+                                .Select(y => new
+                                {
+                                  date = y.Key,
+                                  count = db.Posts.GetAll()
+                                                 .ToList()
+                                                 .Where(post => post.ApplicationUserId == userId && post.PostDate.Month == y.Key)
+                                                 .ToList()
+                                                 .Count.ToString()
+                                });
+
+            return Json(posts, JsonRequestBehavior.AllowGet);
+        }
+
+        
+        //Get Replies Per User
+        public JsonResult GetReplies()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = db.Users.GetUserById(userId);
+            var replies = db.Replies.GetAll()
+                                    .ToList()
+                                    .Where(reply => reply.ApplicationUser == user)
+                                    .GroupBy(x => x.PostDate.Month)
+                                    .Select(y => new
+                                    {
+                                      date = y.Key,
+                                      count = db.Posts.GetAll()
+                                                     .ToList()
+                                                     .Where(reply => reply.ApplicationUser == user && reply.PostDate.Month == y.Key)
+                                                     .ToList()
+                                                     .Count.ToString()
+                                    }).OrderBy(x=>x.date);
+
+        
+            return Json(replies, JsonRequestBehavior.AllowGet);
+        }
+
+        //Get Replies Count from Posts and Group them By Tags Img.
+        public JsonResult GetTagsPercentage()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = db.Users.GetUserById(userId);
+            var tagsPercentage = db.Posts.GetAll()
+                                          .ToList() //Posts that user replied
+                                          .Where(reply => reply.ApplicationUser == user)
+                                          .GroupBy(post => post.Tags)
+                                          .Select(x => new
+                                          {
+                                             
+                                          });
+
+            return Json(tagsPercentage, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
