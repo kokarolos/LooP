10f3fac78cfcc3070f2fe40b51c2cd1bc06d3d12
ioannis-grek <ioannis_1975@hotmail.com>
2020-05-30 04:43:52 +0300

    added posts per tag page, modified tags page

---
+++ Loop.Web/Scripts/ioannis/postsPerTag.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('PostsPerTagApp', []);
+app.filter("formatDate",
+    function ()
+    {
+        return function (dateFromJson){ return dateFromJson.slice(6, -2) }
+    }
+)
+app.controller("PostsPerTagController", function ($scope, $http, $attrs) {
+    $http.get("/Tags/GetTagById/" + $attrs.tagId).then(
+        function (response) {
+            console.log(response.data[0]);
+            $scope.postsPerTag = response.data[0];
+        }
+        , function (response) {
+            console.log(response.status);
+        }
+    );
+
+    $scope.postFilter = function (post) {
+        if ($scope.model_search === undefined || $scope.model_search === null || $scope.model_search === "" || post === undefined) return true;
+        return post.Title.toLowerCase().includes($scope.model_search.toLowerCase());
+    }
+
+});
